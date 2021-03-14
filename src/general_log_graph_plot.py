e22779f394963250abf0e89a6d7ecb9f45977e10
jupiter3p3 <44353460+jupiter3p3@users.noreply.github.com>
2021-03-14 11:34:05 +0800

    Remove space of surrounding docstring text

--- src/general_log_graph_plot.py
+++ src/general_log_graph_plot.py
@@ -1,6 +1,6 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-''' Version history
+'''Version history
 
 To generatelog visual graph
 # Version|Date       |Author     |Note                                  |
