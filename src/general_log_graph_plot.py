0e6adcfd2046e07f38104060bb16bbe6e52c34ca
jupiter3p3 <44353460+jupiter3p3@users.noreply.github.com>
2021-03-14 11:29:37 +0800

    Add 1 blank line between summary line and description

--- src/general_log_graph_plot.py
+++ src/general_log_graph_plot.py
@@ -1,6 +1,7 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
+''' Version history
+
 To generatelog visual graph
 # Version|Date       |Author     |Note                                  |
 # 0.0.1  |20210217   |jupiter    |Draft version                         |
